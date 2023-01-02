e72d693dd91e9e70e5685ddabfb6b266d684949d
davidho27941 <davidho.prime@gmail.com>
2023-01-02 20:02:10 +0800

    chore: add setup script for building package.

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fantacy",
+    version="0.1.0",
+    author="davidho27941",
+    author_email="davidho.prime@gmail.com",
+    description="A pandas-based technical analysis package for stock market.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'numpy>=1'
+    ]
+)
